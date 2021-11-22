97880de3ebd1b9e6e5fc92897f7c3fd17811e34a
David Choi <choi.david4@gmail.com>
2021-11-22 17:12:19 -0300

    finished

--- src/index.js
+++ src/index.js
@@ -2,88 +2,74 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 
-/*
-User Story #1: I can see an element with id="break-label" that contains a string (e.g. "Break Length"). DONE
+class MyTimer extends React.Component{
+    constructor(props){
+        super(props);
 
-User Story #2: I can see an element with id="session-label" that contains a string (e.g. "Session Length"). DONE
+        this.INTERVALID = 0;
+        this.state = {
+            secondsRemaining: this.props.minutes * 60,
+            mmss: (this.props.minutes <= 9) ? 
+            "0" + this.props.minutes + ":00" : 
+            this.props.minutes + ":00" ,
+            paused: true
+        }
 
-User Story #3: I can see two clickable elements with corresponding IDs: id="break-decrement" and id="session-decrement". DONE
+        this.handleInterval = this.handleInterval.bind(this);
+        this.handlePause = this.handlePause.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
 
-User Story #4: I can see two clickable elements with corresponding IDs: id="break-increment" and id="session-increment". DONE
+    handleInterval(){
+        let secondsRemaining = this.state.secondsRemaining;
+        if(secondsRemaining <= 0){
+            clearInterval(this.INTERVALID);
+        }else{
+            secondsRemaining--;
+            let minutes = parseInt(secondsRemaining / 60);
+            if(minutes <= 9){
+                minutes = "0" + minutes;
+            }
+            let seconds = secondsRemaining - minutes * 60;
+            if(seconds <= 9){
+                seconds = "0" + seconds;
+            }
+            let mmss = minutes + ":" + seconds;
+            this.setState({secondsRemaining, mmss});
+        }
+    }
 
-User Story #5: I can see an element with a corresponding id="break-length", which by default (on load) displays a value of 5. DONE
+    handlePause(){
+        if(this.state.paused){
+            this.INTERVALID = setInterval(this.handleInterval, 1000);
+            this.setState({paused: false});
+        }else{
+            clearInterval(this.INTERVALID);
+            this.setState({paused: true});
+        }
+    }
 
-User Story #6: I can see an element with a corresponding id="session-length", which by default displays a value of 25. DONE
+    handleReset(){
+        clearInterval(this.INTERVALID);
+        this.setState({
+            secondsRemaining: this.props.minutes * 60,
+            mmss: (this.props.minutes <= 9) ? 
+            "0" + this.props.minutes + ":00" : 
+            this.props.minutes + ":00" ,
+            paused: true
+        });
+    }
 
-User Story #7: I can see an element with a corresponding id="timer-label", that contains a string indicating a session is initialized (e.g. "Session"). DONE
-
-User Story #8: I can see an element with corresponding id="time-left". NOTE: Paused or running, the value in this field should always be displayed in mm:ss format (i.e. 25:00). DONE
-
-User Story #9: I can see a clickable element with a corresponding id="start_stop". DONE
-
-User Story #10: I can see a clickable element with a corresponding id="reset". DONE
-
-User Story #11: When I click the element with the id of reset, any running timer should be stopped, the value within id="break-length" should return to 5, the value within id="session-length" should return to 25, and the element with id="time-left" should reset to its default state. 
-
-User Story #12: When I click the element with the id of break-decrement, the value within id="break-length" decrements by a value of 1, and I can see the updated value. DONE
-
-User Story #13: When I click the element with the id of break-increment, the value within id="break-length" increments by a value of 1, and I can see the updated value. DONE
-
-User Story #14: When I click the element with the id of session-decrement, the value within id="session-length" decrements by a value of 1, and I can see the updated value. DONE
-
-User Story #15: When I click the element with the id of session-increment, the value within id="session-length" increments by a value of 1, and I can see the updated value. DONE
-
-User Story #16: I should not be able to set a session or break length to <= 0. DONE
-
-User Story #17: I should not be able to set a session or break length to > 60. DONE
-
-User Story #18: When I first click the element with id="start_stop", the timer should begin running from the value currently displayed in id="session-length", even if the value has been incremented or decremented from the original value of 25. 
-
-User Story #19: If the timer is running, the element with the id of time-left should display the remaining time in mm:ss format (decrementing by a value of 1 and updating the display every 1000ms).
-
-User Story #20: If the timer is running and I click the element with id="start_stop", the countdown should pause.
-
-User Story #21: If the timer is paused and I click the element with id="start_stop", the countdown should resume running from the point at which it was paused.
-
-User Story #22: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-label should display a string indicating a break has begun.
-
-User Story #23: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), a new break countdown should begin, counting down from the value currently displayed in the id="break-length" element. 
-
-User Story #24: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-label should display a string indicating a session has begun. 
-
-User Story #25: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), a new session countdown should begin, counting down from the value currently displayed in the id="session-length" element.
-
-User Story #26: When a countdown reaches zero (NOTE: timer MUST reach 00:00), a sound indicating that time is up should play. This should utilize an HTML5 audio tag and have a corresponding id="beep".
-
-User Story #27: The audio element with id="beep" must be 1 second or longer. DONE
-
-User Story #28: The audio element with id of beep must stop playing and be rewound to the beginning when the element with the id of reset is clicked.
-*/
-
-/*
-Elementos obligatorios
-
-id: descripcion
-
-break-label: string "break length" DONE
-session-label: string "session length" DONE
-
-break-decrement: break down button DONE
-session-decrement: session down button DONE
-break-increment: break up button DONE
-session-increment: session up button DONE
-
-break-length: break length number DONE
-session-length: session length number DONE
-
-timer-label: session/break string DONE
-time-left: mm:ss format DONE
-
-start_stop: start/stop button DONE
-reset: reset button DONE
-
-beep: html5 audio element DONE
-*/
+    render(){
+        return(
+            <div>
+                <h2>{this.state.mmss}</h2>
+                <button onClick={this.handlePause} >Start / Pause</button>
+                <button onClick={this.handleReset} >Reset</button>
+            </div>
+        );
+    }
+}
 
 class TimeLengthControl extends React.Component{
     constructor(props){
@@ -124,37 +110,164 @@ class TimeLengthControl extends React.Component{
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.handleDecrement = this.handleDecrement.bind(this);
-        this.handleIncrement = this.handleIncrement.bind(this);
+        
+        this.INTERVALID = 0;
+        this.beep;
         this.state = {
             break: 5,
             session: 25,
             duringSession: true,
+            secondsRemaining: 3,
+            paused: true,
         };
+
+        this.handleDecrement = this.handleDecrement.bind(this);
+        this.handleIncrement = this.handleIncrement.bind(this);
+        this.handleInterval = this.handleInterval.bind(this);
+        this.handlePause = this.handlePause.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+
+    componentDidMount(){
+        this.beep = document.getElementById("beep");
     }
 
     handleDecrement(isBreak){
-        if(isBreak){
-            if(this.state.break > 1){
-                this.setState({break: this.state.break-1});
-            }
-        }else{
-            if(this.state.session > 1){
-                this.setState({session: this.state.session-1});
+        if(this.state.paused){
+            if(isBreak){
+                if(this.state.break > 1){
+                    this.setState({
+                        break: this.state.break-1,
+                    });
+                }
+            }else{
+                if(this.state.session > 1){
+                    if(this.state.duringSession){
+                        this.setState(
+                            {
+                                session: this.state.session - 1,
+                                secondsRemaining: (this.state.session - 1) * 60
+                            }
+                        );
+                    }else{
+                        this.setState(
+                            {
+                                session: this.state.session - 1,
+                            }
+                        );
+                    }
+                }
             }
         }
     }
 
     handleIncrement(isBreak){
-        if(isBreak){
-            if(this.state.break < 60){
-                this.setState({break: this.state.break+1});
-            }
-        }else{
-            if(this.state.session < 60){
-                this.setState({session: this.state.session+1});
+        if(this.state.paused){
+            if(isBreak){
+                if(this.state.break < 60){
+                    this.setState({
+                        break: this.state.break+1,
+                    });
+                }
+            }else{
+                if(this.state.session < 60){
+                    if(this.state.duringSession){
+                        this.setState(
+                            {
+                                session: this.state.session + 1,
+                                secondsRemaining: (this.state.session + 1) * 60
+                            }
+                        );
+                    }else{
+                        this.setState(
+                            {
+                                session: this.state.session + 1,
+                            }
+                        );
+                    }
+                }
+                
             }
         }
+    }
+
+    handleInterval(){
+        let secondsRemaining = this.state.secondsRemaining;
+        if(secondsRemaining <= 0){
+            this.playBeep();
+            clearInterval(this.INTERVALID);
+            if(this.state.duringSession){
+                this.startBreak();
+            }else{
+                this.startSession();
+            }
+            
+        }else{
+            secondsRemaining--;
+            this.setState({secondsRemaining});
+        }
+    }
+
+
+    handlePause(){
+        if(this.state.paused){
+            this.INTERVALID = setInterval(this.handleInterval, 1000);
+            this.setState({paused: false});
+        }else{
+            clearInterval(this.INTERVALID);
+            this.setState({paused: true});
+        }
+    }
+
+    handleReset(){
+        clearInterval(this.INTERVALID);
+        this.setState({
+            break: 5,
+            session: 25,
+            duringSession: true,
+            secondsRemaining: 25 * 60,
+            paused: true,
+        });
+        this.beep.pause();
+        this.beep.load();
+    }
+
+
+    // AUX FUNCTIONS
+
+    startBreak(){
+        this.setState({
+            duringSession: false,
+            secondsRemaining: this.state.break * 60,
+            paused: false,
+        });
+        this.INTERVALID = setInterval(this.handleInterval, 1000);
+    }
+
+    startSession(){
+        this.setState({
+            duringSession: true,
+            secondsRemaining: this.state.session * 60,
+            paused: false,
+        });
+        this.INTERVALID = setInterval(this.handleInterval, 1000);
+    }
+
+
+    playBeep(){
+        this.beep.play();
+    }
+
+    processSeconds(secondsRemaining){
+        let minutes = parseInt(secondsRemaining / 60);
+        if(minutes <= 9){
+            minutes = "0" + minutes;
+        }
+        let seconds = secondsRemaining - minutes * 60;
+        if(seconds <= 9){
+            seconds = "0" + seconds;
+        }
+        return minutes + ":" + seconds;
     }
 
     render(){
@@ -178,19 +291,23 @@ class App extends React.Component{
                 </div>
                 <div className="timer-container">
                     <h2 id="timer-label">{statusString}</h2>
-                    <p id="time-left">{this.state.session}:00</p>
+                    <p id="time-left">{this.processSeconds(this.state.secondsRemaining)}</p>
                     <div id="controls">
                         <button 
                         id="start_stop"
+                        onClick={this.handlePause}
                         >
-                            Start / Stop
+                            Start / Pause
                         </button>
-                        <button id="reset">Reset</button>
+                        <button 
+                         id="reset"
+                         onClick={this.handleReset}
+                        >Reset</button>
                     </div>
                 </div>
                 <audio 
                     id="beep"
-                    src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+                    src="http://soundfxcenter.com/television/dragon-ball-z/8d82b5_Dragon_Ball_Z_Gohan_Yells_Sound_Effect.mp3"
                 />
             </div>
         );
@@ -202,6 +319,16 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+
+/*
+AUDIOS:
+    annoying beep: 
+    https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav
+    
+    gohan:
+    http://soundfxcenter.com/television/dragon-ball-z/8d82b5_Dragon_Ball_Z_Gohan_Yells_Sound_Effect.mp3
+
+*/
 
 //Pensar como implementar el start/stop
 /*
--- src/test.js
+++ src/test.js
@@ -1,4 +1,27 @@
-let myString = "naruto";
-let myStringUpper = myString[0].toUpperCase() + myString.slice(1);
+//building a timer from scratch
 
-console.log(myStringUpper);
+const INITIAL_MINUTES = 1;
+
+let secondsRemaining = INITIAL_MINUTES * 60;
+
+const INTERVALID = setInterval(handleInterval, 1000);
+
+function handleInterval(){
+    let minutes = parseInt(secondsRemaining / 60);
+    if(minutes <= 9){
+        minutes = "0"+minutes;
+    }
+    let seconds = secondsRemaining - minutes * 60;
+    if(seconds <= 9){
+        seconds = "0"+seconds;
+    }
+
+    console.log(minutes + ":" + seconds);
+
+    if(minutes === "00" && seconds === "00"){
+        console.log("Timer ended");
+        clearInterval(INTERVALID);
+    }else{
+        secondsRemaining--;
+    }
+}
