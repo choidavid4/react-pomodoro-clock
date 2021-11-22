a4b89530e9a8255a9d445931fbeface75bedf22d
David Choi <choi.david4@gmail.com>
2021-11-22 17:54:05 -0300

    edited starting seconds

--- src/index.js
+++ src/index.js
@@ -118,7 +118,7 @@ class App extends React.Component{
             break: 5,
             session: 25,
             duringSession: true,
-            secondsRemaining: 3,
+            secondsRemaining: 25*60,
             paused: true,
         };
 
